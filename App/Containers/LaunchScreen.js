c052c8fb6485e8d1b74317e62ec6cfcdbe00bae0
Jimmy Zhang <jimzhang93@gmail.com>
2017-12-05 11:14:15 -0500

    initial push

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -18,7 +18,7 @@ export default class LaunchScreen extends Component {
           <View style={styles.section} >
             <Image source={Images.ready} />
             <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
+              Naz is a noob
             </Text>
           </View>
 
