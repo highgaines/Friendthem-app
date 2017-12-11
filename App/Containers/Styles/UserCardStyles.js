9660d79f2d4ea7a6bacb9706aebf88b7a417f428
Naz Khan <nazmuskhan@yahoo.com>
2017-12-11 16:41:06 -0500

    working on navigation and input form

--- App/Containers/Styles/UserCardStyles.js
+++ App/Containers/Styles/UserCardStyles.js
@@ -4,6 +4,7 @@ import { Metrics, ApplicationStyles, Fonts } from '../../Themes/'
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   card: {
+    alignItems: 'center',
     backgroundColor: 'white',
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
