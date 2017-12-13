c3b9613ec25e48c62b8a8ca88061ffceb7890e7a
EoghanL <eleddy521@gmail.com>
2017-12-13 12:16:12 -0500

    adds header section for user profile

---
+++ App/Containers/Styles/UserProfileStyles.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native'
+import { Metrics, ApplicationStyles, Fonts } from '../../Themes/'
+
+export default StyleSheet.create({
+  ...ApplicationStyles.screen,
+  profile: {
+
+  },
+  profileHeader: {
+    height: 200,
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 30
+  },
+  profHeaderTop: {
+    height: 125,
+    flexDirection: 'row'
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    borderWidth: 3,
+    borderColor: '#ffffff'
+  },
+  profileSubtext: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    backgroundColor: 'transparent',
+    color: '#ffffff'
+  },
+  linearGradient: {
+    flex: 1
+  },
+  phoneIcon: {
+    height: 35,
+    width: 35,
+    marginTop: 45,
+    marginRight: 20,
+    borderWidth: 2,
+    borderColor: '#ffffff',
+    borderRadius: 17.5
+  },
+  mailIcon: {
+    height: 35,
+    width: 35,
+    marginTop: 45,
+    marginLeft: 20,
+    borderWidth: 2,
+    borderColor: '#ffffff',
+    borderRadius: 17.5
+  }
+})
