e54b05eced243aa04037599786e46d557499f3dd
Naz Khan <nazmuskhan@yahoo.com>
2018-02-12 14:18:01 -0500

    removed flex

--- App/Containers/ProfileScreen/UserProfileScreen.js
+++ App/Containers/ProfileScreen/UserProfileScreen.js
@@ -170,7 +170,6 @@ class UserProfileScreen extends Component {
           colors={['#e73436', '#b31c85', '#9011ba', '#5664bd', '#2aa5c0']}
           start={{x: 0.0, y: 0.0}} end={{x: 1.0, y: 1.0}}
           locations={[0.1, 0.3, 0.5, 0.7, 1.0]}
-          style={styles.linearGradient}
           >
           <PickSocialMediaModal
             triggerModal={this.triggerFriendster}
