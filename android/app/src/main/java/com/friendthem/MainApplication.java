cfd3c6dbc642d8f8cb00ebffd7c564a5e85b7435
Naz Khan <nazmuskhan@yahoo.com>
2018-02-27 15:30:37 -0500

    started set up for aws and image picker library

--- android/app/src/main/java/com/friendthem/MainApplication.java
+++ android/app/src/main/java/com/friendthem/MainApplication.java
@@ -3,6 +3,7 @@ package com.friendthem;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.microsoft.codepush.react.CodePush;
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new ReactNativeOneSignalPackage(),
             new ReactNativeContacts(),
             new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
